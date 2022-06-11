8583ae0fbc28a838122262c4c4cf004738bf814b
Josh83821 <98107328+js2820@users.noreply.github.com>
2022-06-11 14:01:43 +0100

    Some progress
    Made a little progress. I think it works I didn't bother to multiply the matrix by V/deltah

--- Sheet 2b/Q1.py
+++ Sheet 2b/Q1.py
@@ -4,20 +4,30 @@ import matplotlib.pyplot as plt
 
 n = 50
 u0 = 1
+t = np.linspace(0,10)
 
-u = np.linspace(0,5, num = n+1)
+
+u = np.zeros(n+1)
+u[0] = u0
+u[n] = u0
 
 # Get A
 A = np.zeros((n+1, n+1))
-A[0, 0] = u0
-A[n, n] = u0
+A[0, 0] = 1
+A[n, n] = 1
 for i in range(1, n):
     A[i, i-1] = 1
     A[i, i] = -2
     A[i, i+1] = 1
 
-def model(u):
-    dudt = A*u
+def model(u, t):
+    dudt = A@u
     return dudt
 
-print(A@u)
+z = odeint(model, y0 = u, t = t)
+
+# plot results
+plt.plot(t,z,'r-',linewidth=2,label='u')
+plt.xlabel('time')
+plt.ylabel('Difference')
+plt.show()
