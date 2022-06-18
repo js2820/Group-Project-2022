94759fa925a2f0ddf0d7f571dc75f26cc3c76b9a
Shepherd <js2820@ic.ac.uk>
2022-06-18 17:47:47 +0100

    I'm close

--- Real/CY.py
+++ Real/CY.py
@@ -11,16 +11,23 @@ llambda=1
 N=1
 a=1
 
+eta = y**2
 #eta = eta_inf + (eta0 - eta_inf) * (1 + (llambda * u0)**a)**((n-1)/2)
 
-d1 = np.zeros((n+1, n+1))
-d2 = np.zeros((n+1, n+1))
-for i in range(1, n):
+d1 = np.zeros((n, n))
+d2 = np.zeros((n, n))
+for i in range(1, n-1):
     d2[i, i-1] = 1
     d2[i, i] = -2
     d2[i, i+1] = 1
     #d1:
     d1[i, i-1] = -1
-    d1[i, i+1] = 1    
+    d1[i, i+1] = 1
 
-print(y, d1, d2)
+
+B = np.matmul(np.matmul(d1, eta), d1)
+A = np.matmul(eta, d2)
+
+print(B)
+#print(B)
+#Mat = A + B
