aaa0fbba7aee132af1dbd43338305b659298a1e2
Shepherd <js2820@ic.ac.uk>
2022-06-18 16:03:58 +0100

    Starting

---
+++ Real/CY.py
@@ -0,0 +1,11 @@
+
+
+#def Carreau-Yasuda(eta0=2, eta_inf=1, llambda=1, u0, n=1, a=1):
+eta0=2
+eta_inf=1
+llambda=1
+u0 = ?
+n=1
+a=1
+
+eta = eta_inf + (eta0 - eta_inf) * (1 + (llambda* u0)**a)**((n-1)/2)
