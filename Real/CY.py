c691ed57942f88e65f6760c048aea692e91fdbc3
Shepherd <js2820@ic.ac.uk>
2022-06-19 15:37:00 +0100

    Looks like it sorted worked??

--- Real/CY.py
+++ Real/CY.py
@@ -1,42 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#def Carreau-Yasuda(eta0=2, eta_inf=1, llambda=1, u0, n=1, a=1):
-n = 50
+
+def get_finDiff_matrices(n):
+    d1 = np.zeros((n, n))
+    d2 = np.zeros((n, n))
+    for i in range(1, n-1):
+        d2[i, i-1] = 1
+        d2[i, i] = -2
+        d2[i, i+1] = 1
+        #d1:
+        d1[i, i-1] = 1
+        d1[i, i+1] = -1
+    
+    return d1, d2
+
 H = 1
 y = np.linspace(0, H, num=n)
-eta0=2
-eta_inf=1
-llambda=1
-#u0 = Some function of y
-N=2
-a=1
-alpha = 0.5
-eta = y**2
-#eta = eta_inf + (eta0 - eta_inf) * (1 + (llambda * u0)**a)**((n-1)/2)
 
-d1 = np.zeros((n, n))
-d2 = np.zeros((n, n))
-for i in range(1, n-1):
-    d2[i, i-1] = 1
-    d2[i, i] = -2
-    d2[i, i+1] = 1
-    #d1:
-    d1[i, i-1] = -1
-    d1[i, i+1] = 1
-
-
-Result = np.ones(n)
-Result[0] = 0
-Result[n-1] = 0
-
-
-eps = 0.1
-diff = eps + 1
-
-uprev = y**2
+uthing = -0.5 * y**2 - H * 0.5 * y
 
 def Carreau_Yasuda(uprev, eps, eta0=2, eta_inf=1, llambda=1, n=50, a=1, N=2, alpha = 0.5):
+    
+    Result = np.ones(n)
+    Result[0] = 0
+    Result[n-1] = 0
+    d1, d2 = get_finDiff_matrices(n)
+
     diff = eps+1
     while diff > eps:
         #Get eta, calculate the derivative and set the values along the diagonal
@@ -53,7 +43,6 @@ def Carreau_Yasuda(uprev, eps, eta0=2, eta_inf=1, llambda=1, n=50, a=1, N=2, alp
         Mat = A + B
         Mat[0,0] = 1
         Mat[n-1,n-1] = 1
-        print(Mat)
 
         #Find our uhat
         uhat=np.linalg.solve(Mat, Result)
@@ -61,13 +50,12 @@ def Carreau_Yasuda(uprev, eps, eta0=2, eta_inf=1, llambda=1, n=50, a=1, N=2, alp
         unew = alpha * uprev + (1-alpha) * uhat
         diff = np.linalg.norm(unew - uprev)
         uprev = unew
-        print(unew, diff)
+
     return unew
 
-U = Carreau_Yasuda(uprev = y**2, eps=0.1)
-#Mat = A + B
+U = Carreau_Yasuda(uprev = uthing, eps=1e-5, alpha = 0.9)
 
-plt.plot(y, U,'r-',linewidth=2,label='u1')
+plt.plot(y, U,'r-',linewidth=2,label='u')
 plt.xlabel('Y')
 plt.ylabel('Velocity')
 plt.show()
